e307032668c8da6f0a2c08bd54f1f54812b21602
saurabhbiswas <saurabhbisastech@gmail.com>
2024-09-29 08:38:06 +0530

    Added ARIA label  for  content
    Added ARIA label  for  content

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -35,7 +35,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
   buttonText,
   footerContent,
 }) => (
-  <form className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full" onSubmit={onSubmit}>
+  <form className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full" onSubmit={onSubmit} aria-labelledby="auth-form-title">
     {serverError && <p className="text-red-500 mb-4">{serverError}</p>}
     {setName && (
       <>
@@ -45,8 +45,13 @@ const AuthForm: React.FC<AuthFormProps> = ({
           value={name}
           onChange={(e) => setName?.(e.target.value)}
           className="w-full p-2 mb-1 border border-gray-300 rounded"
+          aria-invalid={!!nameError}
+          aria-describedby={nameError ? 'name-error' : undefined}
         />
-        {nameError && <p className="text-red-500 mb-2 font-custom text-tiny">{nameError}</p>}
+        {nameError && <p className="text-red-500 mb-2 font-custom text-tiny"
+           role="alert"
+           aria-live="assertive"
+        >{nameError}</p>}
       </>
     )}
     <input
@@ -55,25 +60,35 @@ const AuthForm: React.FC<AuthFormProps> = ({
       value={email}
       onChange={(e) => setEmail(e.target.value)}
       className="w-full p-2 mb-1 border border-gray-300 rounded"
+      aria-invalid={!!emailError}
+      aria-describedby={emailError ? 'email-error' : undefined}
+      required
+
     />
-    {emailError && <p className="text-red-500 mb-2 font-custom text-tiny">{emailError}</p>}
+    {emailError && <p className="text-red-500 mb-2 font-custom text-tiny" 
+    role="alert"
+    aria-live="assertive">{emailError}</p>}
     <input
       type="password"
       placeholder="Password"
       value={password}
       onChange={(e) => setPassword(e.target.value)}
       className="w-full p-2 mb-1 border border-gray-300 rounded"
+      aria-invalid={passwordErrors && passwordErrors.length > 0}
+      aria-describedby={passwordErrors && passwordErrors.length > 0 ? 'password-errors' : undefined}
+      required
     />
     {/* Display password errors for Signup */}
     {passwordErrors && passwordErrors.length > 0 && (
-      <ul className="text-red-500 mb-2 list-none list-inside font-custom text-tiny">
+      <ul className="text-red-500 mb-2 list-none list-inside font-custom text-tiny" role="alert"
+          aria-live="assertive">
         {passwordErrors.map((error, index) => (
           <li key={index}>{error}</li>
         ))}
       </ul>
     )}
     
-    <button type="submit" className="bg-green-500 text-white w-full p-2 rounded">
+    <button type="submit" className="bg-green-500 text-white w-full p-2 rounded" aria-label={buttonText}>
       {buttonText}
     </button>
 
