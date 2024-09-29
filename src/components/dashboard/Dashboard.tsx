e8304197b8be43fbbbc18b896eefa290a77f02a0
saurabhbiswas <saurabhbisastech@gmail.com>
2024-09-29 08:45:07 +0530

    Updated dashboard content
    Updated dashboard content

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import { useAuthContext } from '../../context/AuthContext';
 import useAuth from '../../hooks/useAuth';
 
 const Dashboard: React.FC = () => {
-  const { state } = useAuthContext();
+ 
   const { logout } = useAuth();
 
   const handleLogout = () => {
@@ -13,7 +13,6 @@ const Dashboard: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full text-center">
-        <h1 className="text-2xl font-bold mb-6"> {state.user ? state.user.name : 'User'}!</h1>
         <p className="text-gray-600">Welcome to the application.</p>
          <button onClick={handleLogout} className="mt-4 p-2 bg-red-500 text-white rounded">
         Logout
